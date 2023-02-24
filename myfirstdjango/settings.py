8e92802407f06f8ab0db2e7b1e09fce6fe17d00a
samuelwigren <samuel.wigren@gsfacket.se>
2023-02-24 14:56:16 +0000

    Change config in setting.py

--- myfirstdjango/settings.py
+++ myfirstdjango/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'myfirstdjango.wsgi.application'
 #}
 
 DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
  }
 
 # Password validation
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'Cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE ='Cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
